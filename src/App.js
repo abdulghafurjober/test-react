bd87fc454aaf4e3a1aae8f0080c420492aa417d9
abdulghafurjober <abdulghafurjober@hotmail.com>
2021-05-24 22:02:53 +0800

    my first initialization

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://i.guim.co.uk/img/media/fbb1974c1ebbb6bf4c4beae0bb3d9cb93901953c/80_0_2400_1440/master/2400.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=ede2b27f1cea7c3be30b938195c0cc5c" className="App-logo" alt="logo" />
+        <p>
+          Abdul Ghafur Jober
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
